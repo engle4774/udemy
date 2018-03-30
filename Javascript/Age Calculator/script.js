28c40d771a6ea47000a8523d8cba568acf0e6e6a
engle474 <austinengle4774@gmail.com>
2018-03-30 14:27:03 -0600

    update Mar 30 14:26

---
+++ Javascript/Age Calculator/script.js
@@ -0,0 +1,5 @@
+var age = prompt("What is your age?");
+
+var ageInDays = age * 365.25;
+
+alert(age + " years is roughly " + ageInDays + " days")
---
+++ Javascript/JS Stalker Exercise/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var age = prompt("What is your age?");
+
+var fullName = firstName + " " + lastName
+
+console.log("Your full name is " + fullName);
+console.log("You are " + age + " years old");
