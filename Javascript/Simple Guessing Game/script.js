6b647164fd7fe6fec40a527d47a6a3bcfa35dbd3
Austin Engle <austin.engle@wgu.edu>
2018-03-31 21:22:29 -0600

    Finished Simple Guessing Game

--- Javascript/Simple Guessing Game/script.js
+++ Javascript/Simple Guessing Game/script.js
@@ -1,18 +1,20 @@
 //create secretNumber
 var secretNumber = 4;
+
 //ask user for guess
-var guess = prompt("What is the Secret Number?");
-//check guess
+var guess = Number(prompt("Guess a number"));
 
 //if guess is correct
 if(guess === secretNumber) {
-    alert("That is Correct");
+    alert("YOU GOT IT RIGHT");
 }
-//If guess is to low
-else if(guess < secretNumber) {
-    alert("Sorry that is to low, Please try again");
-}
+
 //If guess is to high
-else(guess > secretNumber); {
+else if(guess > secretNumber){
     alert("Sorry that is to high, Please try again");
+}
+
+//If guess is to low
+else{
+    alert("Sorry that is to low, Please try again");
 }
